aeddb479c03f36cca8605abbe18748428c5a0400
Colin Gillespie <csgillespie@gmail.com>
2013-02-14 12:27:59 +0000

    Bug fix in lines and points command.

--- R/lines-methods.R
+++ R/lines-methods.R
@@ -5,7 +5,7 @@ setMethod("lines",
           definition = function(x, length.out=1000, ...) {
               x_values = x$pl_data$x
               x_axs = lseq(x$xmin, max(x_values), length.out)
-              y = dist_cdf(m, x_axs, FALSE)
+              y = dist_cdf(x, x_axs, FALSE)
               lines(x_axs, y, ...)
               invisible(data.frame(x=x_axs, y=y))
           }
