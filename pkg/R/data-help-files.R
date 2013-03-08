84a7f5b2619329468dea9a55d2bc01ebc33bdb91
Colin Gillespie <csgillespie@gmail.com>
2013-03-08 18:24:40 +0000

    Removing the blackouts data set

--- pkg/R/data-help-files.R
+++ pkg/R/data-help-files.R
@@ -1,11 +1,3 @@
-#' @name blackouts
-#' @title Electrical blackout
-#' @description The numbers of customers affected in electrical blackouts in the United States between 1984 and 2002.
-#' @docType data
-#' @format A vector
-#' @source M. E. J. Newman, "Power laws, Pareto distributions and Zipf's law." Contemporary Physics 46, 323 (2005). See http://tuvalu.santafe.edu/~aaronc/powerlaws/data.htm for further details.
-NULL
-
 #' @name moby
 #' @aliases moby_sample
 #' @title Moby Dick word count
