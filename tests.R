20c0672a1ca423a82e743e9900d762b47dae9182
Colin Gillespie <csgillespie@gmail.com>
2013-05-02 22:01:23 +0100

    Adding poisson test

--- tests.R
+++ tests.R
@@ -1,9 +1,10 @@
 library(poweRlaw)
 library(testthat)
-test_dir("tests/")
+#test_dir("tests/")
 test_file("tests/test_discrete_powerlaw.R")
 test_file("tests/test_discrete_clauset.R")
 test_file("tests/test_lognormal_discrete.R")
+test_file("tests/test_poisson.R")
 
 
 
