4185808f254a8b44696fa4b6686635607ee819f5
Colin Gillespie <csgillespie@gmail.com>
2013-05-03 21:53:47 +0100

    Linear interpolate between points for plotting

--- pkg/R/lines-methods.R
+++ pkg/R/lines-methods.R
@@ -2,7 +2,11 @@
 #' @aliases lines,distribution-method
 setMethod("lines",
           signature = signature(x="distribution"),
-          definition = function(x, cut=FALSE, length.out=10, ...) {
+          definition = function(x, 
+                                cut=FALSE, 
+                                length.out=10, 
+                                draw=TRUE,
+                                ...) {
             scale = 1
             xmin = x$getXmin()
             x_values = x$dat
@@ -12,19 +16,42 @@ setMethod("lines",
             
             y = dist_cdf(x, x_axs, FALSE)
             if(!cut) {
-              x$setXmin(1)
-              d_cdf = dist_data_cdf(x, lower_tail=FALSE)
-              ##If CTN, then in theory we can't have
-              ##equal data points. But in practice...
-              ##Take the min
               if(is(x,"discrete_distribution")) 
-                scale = d_cdf[unique(x$internal[["dat"]])==xmin]
-              else
-                scale = max(d_cdf[x$internal[["dat"]]==xmin])
+                x$setXmin(1)
+              else 
+                x$setXmin(0)
+              d_cdf = dist_data_cdf(x, lower_tail=FALSE)
+ 
+              ##Linear interprolate between cdf points
+              if(is(x,"discrete_distribution")) {
+                dif = x$internal[["values"]] - xmin
+                upper = which(dif > 0)[1]
+                lower = upper - 1
+                x_dif = x$internal[["values"]][lower] - 
+                  x$internal[["values"]][upper]
+                y_dif = ifelse(lower, d_cdf[lower], 0) - d_cdf[upper]
+                
+                scale = ifelse(lower, d_cdf[lower], 0) + 
+                  y_dif*(xmin - x$internal[["values"]][lower])/x_dif
+                
+                
+              } else {
+                dif = x$internal[["dat"]] - xmin
+                upper = which(dif > 0)[1]
+                lower = upper - 1
+                x_dif = x$internal[["dat"]][lower] - x$internal[["dat"]][upper]
+                y_dif = ifelse(lower, d_cdf[lower], 0) - d_cdf[upper]
+                
+                scale = ifelse(lower, d_cdf[lower], 0) + 
+                  y_dif*(xmin - x$internal[["dat"]][lower])/x_dif
+                
+              }
+              
               x$setXmin(xmin)
             }
             y = y*scale
-            lines(x_axs, y, ...)
+            if(draw)
+              lines(x_axs, y, ...)
             invisible(data.frame(x=x_axs, y=y))
           }
 )
--- pkg/R/points-methods.R
+++ pkg/R/points-methods.R
@@ -2,12 +2,15 @@
 #' @aliases points,distribution-method
 setMethod("points",
           signature = signature(x="distribution"),
-          definition = function(x, length.out=1000, ...) {
-            x_values = x$dat
-            x_axs = lseq(x$xmin, max(x_values), length.out)
-            y = dist_cdf(x, x_axs, FALSE)
-            points(x_axs, y, ...)
-            invisible(data.frame(x=x_axs, y))
+          definition = function(x, 
+                                cut=FALSE, 
+                                length.out=10, 
+                                draw=TRUE,
+                                ...) {
+            d = lines(x, cut, length.out, FALSE, ...)
+            if(draw)
+              points(d$x, d$y, ...)
+            invisible(d)
           }
 )
 
