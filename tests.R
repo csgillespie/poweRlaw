57e7572d59cd429f806e1edb9b81a371117c9c2f
Colin Gillespie <csgillespie@gmail.com>
2013-05-11 13:08:10 +0100

    Adding bootstrap test functions

--- tests.R
+++ tests.R
@@ -7,3 +7,7 @@ test_file("tests/test_dist_data_cdf.R")
 
 test_file("tests/test_estimate_pars.R")
 test_file("tests/test_estimate_xmin.R")
+
+test_file("tests/test_bootstrap.R")
+test_file("tests/test_bootstrap_p.R")
+
---
+++ tests/test_bootstrap_p.R
@@ -0,0 +1,5 @@
+test_that("Testing bootstrap p function", {
+  
+  
+}
+)
