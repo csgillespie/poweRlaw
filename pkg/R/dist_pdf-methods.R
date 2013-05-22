f8637680f19999b44f32457241b3dc694f33e9e6
Colin Gillespie <csgillespie@gmail.com>
2013-05-22 15:22:13 +0100

    Adding dist_pdf methods for all distributions

--- pkg/R/dist_pdf-methods.R
+++ pkg/R/dist_pdf-methods.R
@@ -3,7 +3,103 @@
 setMethod("dist_pdf",
           signature = signature(m="displ"),
           definition = function(m, q=NULL) {
+              xmin = m$getXmin(); pars = m$getPars()
               if(is.null(q)) q = m$dat
+              q = q[q >= m$xmin]
               dpldis(q[q >= m$xmin], m$xmin, m$pars, TRUE)
+          }
+)
+
+#' @rdname dist_pdf-methods
+#' @aliases dist_pdf,dislnorm-method
+setMethod("dist_pdf",
+          signature = signature(m="dislnorm"),
+          definition = function(m, q=NULL) {
+            xmin = m$getXmin(); pars = m$getPars()
+            if(is.null(q)) q = m$dat
+            q = q[q >= m$xmin]
+            dlnorm(q+0.5, pars[1], pars[2], log=TRUE) - 
+              plnorm(xmin-0.5, pars[1], pars[2], lower.tail=FALSE, log.p=TRUE)
+            
+          }
+)
+
+
+#' @rdname dist_pdf-methods
+#' @aliases dist_pdf,dispois-method
+setMethod("dist_pdf",
+          signature = signature(m="dispois"),
+          definition = function(m, q=NULL) {
+            xmin = m$getXmin(); pars = m$getPars()
+            if(is.null(q)) q = m$dat
+            q = q[q >= m$xmin]
+            dpois(q, pars, log=TRUE) - ppois(xmin, pars, lower.tail=FALSE, log.p=TRUE)
+          }
+)
+
+
+
+
+
+
+##################################################################
+##CTN distributions
+##################################################################
+#' @rdname dist_pdf-methods
+#' @aliases dist_pdf,conpl-method
+setMethod("dist_pdf",
+          signature = signature(m="conpl"),
+          definition = function(m, q=NULL) {
+            xmin = m$getXmin(); pars = m$getPars()
+            if(is.null(q)) {
+              q = m$dat
+              n = m$internal[["n"]]; N = length(q)
+              q = q[(N-n+1):N]
+            } else {
+              q[q >= xmin]
+            }
+            dplcon(q, xmin, pars, TRUE)
+          }
+)
+
+
+
+
+#' @rdname dist_pdf-methods
+#' @aliases dist_pdf,conlnorm-method
+setMethod("dist_pdf",
+          signature = signature(m="conlnorm"),
+          definition = function(m, q=NULL) {
+            xmin = m$getXmin(); pars = m$getPars()
+            if(is.null(q)) {
+              q = m$dat
+              n = m$internal[["n"]]; N = length(q)
+              q = q[(N-n+1):N]
+            } else {
+              q[q >= m$xmin]
+            }
+            dlnorm(q, pars[1], pars[2], log=TRUE) - plnorm(xmin, pars[1], pars[2], 
+                                                         lower.tail=FALSE, log.p=TRUE)
+            
+          }
+)
+
+
+
+#' @rdname dist_pdf-methods
+#' @aliases dist_pdf,conexp-method
+setMethod("dist_pdf",
+          signature = signature(m="conexp"),
+          definition = function(m, q=NULL) {
+            xmin = m$getXmin(); pars = m$getPars()
+            
+            if(is.null(q)) {
+              q = m$dat
+              n = m$internal[["n"]]; N = length(q)
+              q = q[(N-n+1):N]
+            } else {
+              q[q >= m$xmin]
+            }
+            dexp(q, pars, log=TRUE) - pexp(xmin, pars, lower.tail=FALSE, log.p=TRUE)
           }
 )
