99cfb30ae8c0941aae533763f7be8b7c9484b8bd
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2014-02-17 18:12:02 -0800

    Add dist_rand to dislnorm and disexp

--- pkg/R/dist_rand-methods.R
+++ pkg/R/dist_rand-methods.R
@@ -15,3 +15,32 @@ setMethod("dist_rand",
             rplcon(n, m$xmin, m$pars)
           }
 )
+
+#' @rdname dist_rand-methods
+#' @aliases dist_rand,dislnorm-method
+setMethod("dist_rand",
+          signature = signature(m="dislnorm"),
+          definition = function(m, n="numeric") {
+            result <- numeric()
+            min <- log(m$xmin)
+            while (length(result) < n) {
+              candidates <- rnorm(n, m$pars[1], m$pars[2])
+              result <- c(result, candidates[candidates >= min])
+            }
+            floor(exp(result))
+          }
+)
+
+#' @rdname dist_rand-methods
+#' @aliases dist_rand,disexp-method
+setMethod("dist_rand",
+          signature = signature(m="disexp"),
+          definition = function(m, n="numeric") {
+            result <- numeric()
+            while (length(result) < n) {
+              candidates <- rexp(n, m$pars)
+              result <- c(result, candidates[candidates >= m$xmin])
+            }
+            floor(result)
+          }
+)
