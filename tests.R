9f9268c31fb41b0968ce6a675c2800c903c794d8
Colin Gillespie <csgillespie@gmail.com>
2014-02-24 15:09:41 +0000

    Eased overflow problems with disexp and dislnorm

--- tests.R
+++ tests.R
@@ -1,16 +1,7 @@
+##All other tests are included in the package
+## At present, testthat doesn't seem to handle cluster function calls
 library(poweRlaw)
 library(testthat)
-test_dir("tests/")
-test_file("tests/test_dist_ll.R")
-test_file("tests/test_dist_cdf.R")
-test_file("tests/test_dist_data_cdf.R")
-
-test_file("tests/test_estimate_pars.R")
-test_file("tests/test_estimate_xmin.R")
 
 test_file("tests/test_bootstrap.R")
 test_file("tests/test_bootstrap_p.R")
-
-test_file("tests/test_plot.R")
-
-test_file("tests/test_compare_distributions.R")
