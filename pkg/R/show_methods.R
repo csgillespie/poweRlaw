3ee797a6683069c1585d733a2e274718e4354a4e
Colin Gillespie <csgillespie@gmail.com>
2014-05-21 16:30:18 +0100

    Adding distribution show method (resolves #10)

---
+++ pkg/R/show_methods.R
@@ -0,0 +1,17 @@
+#' Generic show method for distribution objects
+#' 
+#' The distribution objects have an internal structure that is used for caching purposes. 
+#' Using the default \code{show} method gives the illusion of duplicate values. 
+#' This show method aims to avoid this confusion.
+#' @param object A distribution object.
+#' @export
+setMethod("show", 
+          signature = signature(object="distribution"),
+          definition = function(object) {
+            msg = paste("Reference class object of class", classLabel(class(object)))
+            message(msg)
+            message('Field "xmin": '); print(object$getXmin())
+            message('Field "pars": '); print(object$getPars())
+            message('Field "no_pars": '); print(object$no_pars)
+          }
+)
