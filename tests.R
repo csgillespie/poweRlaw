ceb5147a384ec5ba614db579916a37d1cd282e1c
Colin Gillespie <csgillespie@gmail.com>
2014-11-14 16:36:18 +0000

    travis tweak

--- tests.R
+++ tests.R
@@ -2,8 +2,5 @@
 install.packages("pkg/", repo=NULL)
 library(testthat)
 
-test_dir("tests/")
-
-test_file("tests/test_estimate_xmin.R")
-test_file("tests/test_dist_cdf.R")
-test_file("tests/test_bootstrap.R")
+y = test_dir("tests/")
+if(y$failed) stop("Errors")
