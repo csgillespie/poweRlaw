475010abe3c7eb9f434f6e168cc4fe5eaa40e853
Colin Gillespie <csgillespie@gmail.com>
2015-02-20 10:31:10 +0000

    Error message tweak

--- pkg/R/checks.R
+++ pkg/R/checks.R
@@ -4,13 +4,13 @@
 check_discrete_data = function(x) {
   if(!all(floor(x) == x)) 
     stop("Your data should be discrete. 
-          Trying using `floor` or `round` before creating a reference object. ")
+          Either use `floor` or `round` on your data set. Or switch to a continuous distribution.")
   
   if(min(x) < 1)
-    stop("Data should be strictly positive (no zeros)")
+    stop("Data should be strictly positive, i.e. no zeros.")
 }
 
 check_ctn_data = function(x) {
   if(min(x) <= 0)
-    stop("Data should be strictly positive (no zeros)")
+    stop("Data should be strictly positive, i.e. no zeros.")
 }
