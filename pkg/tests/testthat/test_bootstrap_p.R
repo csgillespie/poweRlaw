cbeac8d4128ad9323a9666c32f5197d37c462f8f
Colin Gillespie <csgillespie@gmail.com>
2016-03-31 09:57:21 +0100

    1. Reduce no_of_sims to 3 to save time 2. Typo

--- pkg/tests/testthat/test_bootstrap_p.R
+++ pkg/tests/testthat/test_bootstrap_p.R
@@ -3,21 +3,21 @@ test_that("Testing bootstrap p function", {
   Sys.setenv("R_TESTS" = "") 
   x = 1:10
   mt = displ$new(x)
-  bs = bootstrap_p(mt, no_of_sims=5)
+  bs = bootstrap_p(mt, no_of_sims=3)
   expect_is(bs, "bs_p_xmin")
-  expect_equal(dim(bs$bootstraps), c(5, 4))
+  expect_equal(dim(bs$bootstraps), c(3, 4))
   expect_equal(names(bs), c("p", "gof", "bootstraps", "sim_time", "seed", 
                             "package_version", "distance"))
   
   expect_equal(bs$distance, "ks")
 
-  bs = bootstrap_p(mt, no_of_sims=5, distance="reweight")
-  expect_equal(bs$distance, "ks", distance="reweight")
+  bs = bootstrap_p(mt, no_of_sims=3, distance="reweight")
+  expect_equal(bs$distance, "reweight")
   
   mt = conpl$new(x)
-  bs = bootstrap_p(mt, no_of_sims=5)
+  bs = bootstrap_p(mt, no_of_sims=3)
   expect_is(bs, "bs_p_xmin")
-  expect_equal(dim(bs$bootstraps), c(5, 4))
+  expect_equal(dim(bs$bootstraps), c(3, 4))
   expect_equal(names(bs), c("p", "gof", "bootstraps", "sim_time", "seed", 
                             "package_version", "distance"))
   
