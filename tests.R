f1745d2c714e3a0c650f897218878a03aa838dc7
Colin Gillespie <csgillespie@gmail.com>
2018-01-25 14:15:50 +0000

    Removing pkg sub-directory.

--- tests.R
+++
@@ -1,7 +0,0 @@
-## Moved tests from package to avoid BDR's wrath
-install.packages("pkg/", repo=NULL)
-library(poweRlaw)
-library(testthat)
-devtools::check("pkg/")
-y = test_dir("tests/")
-if(any(y$failed)) stop("Errors")
