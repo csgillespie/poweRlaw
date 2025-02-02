5dd00c70b0cc255040698d51a3acdbd78532238e
Colin Gillespie <csgillespie@gmail.com>
2025-02-02 14:41:44 +0000

    chore: roxygen2 update

--- R/powerlaw-package.R
+++ R/powerlaw-package.R
@@ -10,7 +10,6 @@
 #' @references A. Clauset, C.R. Shalizi, and M.E.J. Newman,
 #' "Power-law distributions in empirical data" SIAM Review 51(4), 661-703 (2009).
 #' @aliases poweRlaw powerlaw powerlaw-package
-#' @docType package
 #' @author \email{colin.gillespie@@newcastle.ac.uk}
 #' @keywords package
 #' @seealso <https://github.com/csgillespie/poweRlaw>
